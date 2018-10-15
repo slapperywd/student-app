87a47d80b052cd3cce15f13d985ccccf5fffe7d2
Siarhei Mahlysh <maglish@mail.ru>
2018-10-15 17:30:12 +0300

    Update src/components/HeroesList.js
    Add implementation of new hero creation

--- src/components/HeroesList.js
+++ src/components/HeroesList.js
@@ -2,14 +2,55 @@ import React, { Component } from 'react';
 import HeroListItem from './HeroListItem';
 
 class HeroesList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            heroes: this.props.heroes
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });   
+    }
+
+    handleSubmit(event) {
+       // alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+        var newHero = {
+            id : 55,// this.heroes[this.heroes.length-1].id + 1,
+            name : this.state.value
+        };
+
+        var heroesList = this.state.heroes;
+        console.log(heroesList);
+        heroesList.push(newHero);
+
+        this.setState({heroes : heroesList});
+        this.setState({value : ''});
+    }
+
     render() {
         return (
-            <div class="heroes-list container">
-                <div class="row">
-                    {this.props.heroes.map(heroItem =>
-                        <HeroListItem hero={heroItem}></HeroListItem>
-                    )}
+            <div>
+                <div class="heroes-list container">
+                    <div class="row">
+                        {this.state.heroes.map(heroItem =>
+                            <HeroListItem hero={heroItem}></HeroListItem>
+                        )}
+                    </div>
                 </div>
+                {/* the code below should not be present here. A good pracitse is to create a separate com */}
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         )
     }
